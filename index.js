81fe2e611950d29c954e8a57864557999ebc595f
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-04-12 00:45:25 -0400

    First Draft

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const app = express()
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
-app.get('/', (request, response) => {
-  return response.render('index', { showdata })
+app.get('/', (request, response) => response.render('index', { showdata }))
+
+app.get('/season/:season', (request, response) => {
+  const requestedSeason = parseInt(request.params.season)
+
+  if (requestedSeason <= showdata.seasons.length) {
+    const validSeason = showdata.seasons.find((season) => season.number === requestedSeason)
+
+    return response.render('season', {
+      title: showdata.title,
+      season: validSeason
+    })
+  }
+  else {
+    return response.status(404).send('This Season Does Not Exist')
+  }
 })
 
 app.all('*', (request, response) => {
   return response.sendStatus(404)
 })
 
-app.listen(7777, () => {
-  // Already using 1337 for other project
-  console.log('PORT 7777 is here for you and willing to listen...') // eslint-disable-line no-console
+app.listen(7779, () => {
+  console.log('PORT 7779 is listening...') // eslint-disable-line no-console
 })
